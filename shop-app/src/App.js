0ef501b61e988dce3ee971d10cc87a240ec6927f
Beverly Antwi-Agyei <beverly.antwi-agyei@playtech.com>
2022-01-27 16:48:23 +0000

    added folders and files for components

--- shop-app/src/App.js
+++ shop-app/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
-import Header from './Components/Header';
+import Header from './Components/Homepage/Header';
 
 function App() {
   return (
     <Header name="Store"/>
+
   );
 }
 
