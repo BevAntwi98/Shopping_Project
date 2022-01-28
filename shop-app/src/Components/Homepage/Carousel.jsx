a89477cf9e4bc2ac86ca675ae923eb9760b0891a
Ayisha Alli <alli.ayisha1@gmail.com>
2022-01-28 11:37:56 +0000

    wireframes and carousel

---
+++ shop-app/src/Components/Homepage/Carousel.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Carousel } from "react-bootstrap";
+
+import image1 from "../images/ph1.jpeg"; // Import image
+import image2 from "../images/ph2.jpeg"; // Import image
+import image3 from "../images/ph3.jpeg"; // Import image
+
+function CarouselContainer() {
+  return (
+    <div className="Carousel-container">
+      <Carousel fade={true} pause={false} >
+        <Carousel.Item interval={4000}>
+          <img className="d-block w-100" src={image1} alt="First slide" />
+        </Carousel.Item>
+        <Carousel.Item interval={4000}>
+          <img className="d-block w-100" src={image2} alt="Third slide" />
+        </Carousel.Item>
+        <Carousel.Item interval={4000}>
+          <img className="d-block w-100" src={image3} alt="Third slide" />
+        </Carousel.Item>
+      </Carousel>
+    </div>
+  );
+}
+
+export default CarouselContainer;
+
+// Tutorial Used https://www.youtube.com/watch?v=5RNAg4r_f4c&t=170s
