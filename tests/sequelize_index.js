7269416c0411063616ce65dff8a5d9d3f0d14fca
Ayisha Alli <alli.ayisha1@gmail.com>
2022-02-01 11:15:51 +0000

    tests

--- tests/sequelize_index.js
+++
@@ -1,8 +0,0 @@
-const {Sequelize, DataTypes, Model} = require('sequelize');
-
-const sequelize = new Sequelize('database', 'username', 'password', {
-    dialect: 'sqlite',
-    storage: './shop-items.db'
-});
-
- module.exports={ sequelize, DataTypes, Model };
