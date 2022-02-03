6253466cbb4b5d5076dcd3273f4df72f3d7a6c13
Beverly Antwi-Agyei <beverly.antwi-agyei@playtech.com>
2022-02-03 17:37:32 +0000

    changed store name

--- shop-app/src/Components/Homepage/Header.jsx
+++ shop-app/src/Components/Homepage/Header.jsx
@@ -27,7 +27,7 @@ function Header() {
 
     return (
         <header className='header-container'>
-            <Link className='nav-links' to='/'><h1>Online Store</h1></Link>
+            <Link className='nav-links' to='/'><h1>Tutto</h1></Link>
             <nav className="nav-menu">
                 <Link className='nav-links' to="/">ALL</Link>
                 {
